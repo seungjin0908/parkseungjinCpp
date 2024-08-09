63828f016b1ce01de530383ee8a16d2bb0b1f702
sbscerti <160083857+sbscerti@users.noreply.github.com>
2024-08-09 21:39:48 +0900

    박승진c++

---
+++ Academy/Day1/Day1/Day1/main01.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	printf("Hello World!\n");
+	printf("Hello World!\n");
+	// 줄을 띄우려면 \n은 엔터(줄띄움)의 효과를 가지고있다.
+
+	// 실습
+	// 애국가 1절 출력하기
+	printf("동해물과 백두산이\n");
+	printf("마르고 닳도록\n");
+	printf("하나님이 보우하사\n");
+	printf("우리나라 만세\n");
+	printf("무궁화 삼천리\n");
+	printf("화려강산 대한사람\n");
+	printf("대한으로 길이 보전하세\n");
+}
+// ctrl + f5 로 실행
+
+//  기본적으로 프로그래밍은
+// 항상 (어떤언어든, 어떤 프레임워크든 동일함.)
+// main 함수를 실행함.
+
+// main 함수는 프로그래밍 시작점
+// #include <iostream>
+// iostream << [input output] stream을 다루는 내용을
+//					포함한다.
+// using namespace std;
+// 얘네는 무엇인가?
+// using namespace std; << 얘는 아직 안쓰고 있음. 사실 지워도 무방.
